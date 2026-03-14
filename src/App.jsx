8d9a4df1589ed66cda05dff60c953ca439c30762
rushabhkamdar2 <rushabh.kamdar12@gmail.com>
2026-03-13 22:37:50 -0400

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import { LangProvider } from './components/LangContext'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
@@ -22,6 +23,7 @@ export default function App() {
         <MapSection />
         <Info />
         <Footer />
+        <Analytics />
       </div>
     </LangProvider>
   )
